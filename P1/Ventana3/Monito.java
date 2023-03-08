8de7e7dd0a7f2ed0a9b2310f3838d326bd1a6d24
Ramonst18 <Ramonst18@hotmail.com>
2023-03-08 14:29:11 -0700

    Probando el pausa del programa

--- P1/Ventana3/Monito.java
+++ P1/Ventana3/Monito.java
@@ -1,19 +1,17 @@
 package P1.Ventana3;
 
-
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.File;
 import java.awt.geom.*;
 
-
-public class Monito extends JLabel implements Runnable{
+public class Monito extends JLabel implements Runnable, KeyListener {
     private String url1, url2;
     private ImageIcon icon;
     private File file;
-    private boolean moveStatus = false, pausar;
+    private boolean moveStatus = false, pausar, stop, runStatus = false, bandera = true;
 
-    public Monito(String url1, String url2){
+    public Monito(String url1, String url2) {
         this.url1 = url1;
         this.url2 = url2;
         file = new File(url1);
@@ -23,36 +21,47 @@ public class Monito extends JLabel implements Runnable{
         setIcon(icon);
     }
 
-    public void run(){
+    public void run() {
+        runStatus = true;
+        stop = false;
+
+        file.exists();
+
         for (int x = 10; x < 200; x += 3) {
 
-            //actualizamos los iconos
+            // actualizamos los iconos
             if (moveStatus) {
                 icon = new ImageIcon(this.getClass().getResource(url1));
                 moveStatus = false;
-            }else{
+            } else {
                 icon = new ImageIcon(this.getClass().getResource(url2));
                 moveStatus = true;
             }
 
-            //actualizamos el icono y damos el movimiento
+            // actualizamos el icono y damos el movimiento
             setIcon(icon);
             setBounds(x, 10, 42, 42);
 
-            //frame d:
+            // frame d:
             try {
                 Thread.sleep(100);
             } catch (Exception e) {
                 // TODO: handle exception
             }
 
-            //sincronizacion
+            // sincronizacion, se tiene que poner cada vez lo de abajo cuando se usa un
+            // THread.sleep
             try {
-                synchronized(this){
-                    //Porcion de codigo sincronizada
+                synchronized (this) {
+                    // Porcion de codigo sincronizada
                     while (pausar) {
-                        //si pausar es verdadero, el hilo estara en espera
+                        // si pausar es verdadero, el hilo estara en espera
                         wait();
+                    }
+
+                    if (stop) {
+                        // rompemos el ciclo del for
+                        break;
                     }
                 }
             } catch (Exception e) {
@@ -61,15 +70,48 @@ public class Monito extends JLabel implements Runnable{
         }
     }
 
-    //METODOS SINCRONIZADOS
-    synchronized void pausarHilo(){
+    // METODOS SINCRONIZADOS
+    synchronized void pausarHilo() {
         pausar = true;
-        
+
     }
-    synchronized void reanudarHilo(){
-        
+
+    synchronized void reanudarHilo() {
+        pausar = false;
+        notify(); // Despertamos al thread
     }
-    synchronized void stopHilo(){
-        
+
+    synchronized void stopHilo() {
+        stop = true;
+        pausar = false;
+        notify();
+    }
+
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        if (runStatus) {
+            if (e.getKeyCode() == KeyEvent.VK_Z) {
+                if (bandera) {
+                    pausarHilo();
+                    bandera = false;
+                } else {
+                    reanudarHilo();
+                    bandera = true;
+                }
+            }
+
+            if (e.getKeyCode() == KeyEvent.VK_X) {
+                stopHilo();
+                bandera = true;
+            }
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
     }
 }
--- P1/Ventana3/Ventana4.java
+++ P1/Ventana3/Ventana4.java
@@ -7,6 +7,8 @@ import javax.swing.JButton;
 import java.awt.event.*;
 
 public class Ventana4 extends JFrame{
+    boolean bandera = true;
+
     public Ventana4(){
         initValues();
     }
@@ -15,45 +17,64 @@ public class Ventana4 extends JFrame{
         //Elementos de la ventana
         Monito monito = new Monito("images/mario1.png", "images/mario2.png");
         JButton btnStart = new JButton("Start");
-        JButton btnPause = new JButton("Pause");
-        JButton btnReanudar = new JButton("Reanudar");
-        JButton btnStop = new JButton("Stop");
+        //JButton btnPause = new JButton("Pause");
+        //JButton btnReanudar = new JButton("Reanudar");
+        //JButton btnStop = new JButton("Stop");
 
         //posiciones de los elementos
         monito.setBounds(10, 10, 42, 42);
         btnStart.setBounds(10, 60, 75, 25);
-        btnPause.setBounds(90, 60, 75, 25);
-        btnReanudar.setBounds(170, 60, 75, 25);
-        btnStop.setBounds(250, 60, 75, 25);
+        //btnPause.setBounds(90, 60, 75, 25);
+        //btnReanudar.setBounds(170, 60, 75, 25);
+        //btnStop.setBounds(250, 60, 75, 25);
+
+        //FOCUS
+        btnStart.setFocusable(false);
+        monito.setFocusable(true);
 
         //ActionLisstener
         ActionListener listener = new ActionListener() {
             public void actionPerformed(ActionEvent ae){
 
                 if (ae.getSource() == btnStart) {
-                    
                     Thread t = new Thread(monito);
                     t.start();
                 }
+                /*
                 if (ae.getSource() == btnPause) {
-                    monito.pausarHilo();
+
+                    if (bandera) {
+                        monito.pausarHilo();
+                        bandera = false;
+                    } else{
+                        monito.reanudarHilo();
+                        bandera = true;
+                    }
                 }
+                
+                if (ae.getSource() == btnReanudar) {
+                    monito.reanudarHilo();
+                } 
+                if (ae.getSource() == btnStop) {
+                    monito.stopHilo();
+                }*/
 
             }
         };
 
         //Agregamos los action LIstener
         btnStart.addActionListener(listener);
-        btnPause.addActionListener(listener);
-        btnReanudar.addActionListener(listener);
-        btnStop.addActionListener(listener);
+        monito.addKeyListener(monito);
+        //btnPause.addActionListener(listener);
+        //btnReanudar.addActionListener(listener);
+        //btnStop.addActionListener(listener);
 
         //agregamos los elementos a la ventana
         add(monito);
         add(btnStart);
-        add(btnPause);
-        add(btnReanudar);
-        add(btnStop);
+        //add(btnPause);
+        //add(btnReanudar);
+        //add(btnStop);
 
         //Configuracion de la ventana
         setTitle("Ventana 4");
