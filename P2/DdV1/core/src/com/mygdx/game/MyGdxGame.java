4ef15c56d0edd0c51d64db009bbc54f73f665738
Ramonst18 <Ramonst18@hotmail.com>
2023-04-26 11:51:03 -0700

    Movimiento del personaje completado

--- P2/DdV1/core/src/com/mygdx/game/MyGdxGame.java
+++ P2/DdV1/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,16 +1,22 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class MyGdxGame extends ApplicationAdapter {
 	// Las variables deben de estar en private
 	private SpriteBatch batch;
 	private Texture img;
 	private TextureRegion region;	//sirve para tomar partes de la imagen
+	private Sprite miSprite;
+	private float posX = 300, width, height, velocidad = 100, tiempoT = 0;
+	private boolean derechaIzquierda; //true derecha, false izquierda
 
 	@Override
 	public void create() {
@@ -19,6 +25,15 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch = new SpriteBatch();	//Se utiliza para que las imagenes se usen de manera conjunta
 		img = new Texture("mario1.png");
 		region = new TextureRegion(img, 0,0,32,20);
+		miSprite = new Sprite(img,0,0,42,42);	//Recortamos directamente la imagen
+		miSprite.setPosition(300, 350);
+
+		width = Gdx.graphics.getWidth();	//Obtenemos el ancho
+		height = Gdx.graphics.getWidth();	//obtenemos el alto
+
+		derechaIzquierda = true;
+
+		miSprite.rotate(90);//giramos el sprite
 	}
 
 	@Override
@@ -33,8 +48,68 @@ public class MyGdxGame extends ApplicationAdapter {
 		//Dibujamos en pantalla
 		batch.draw(img, 10, 50, 100,75);	//posicionamos un elemento en pantalla
 		batch.draw(region,10,170,50,50);
+		miSprite.draw(batch);
+		/*
+		//preguntamos la colision con la ventana
+		if (posX == (width - 43)) {
+			derechaIzquierda = false;
+		}else if (posX == 0) {
+			derechaIzquierda = true;
+		}
+
+		//posicion a moverse
+		if (derechaIzquierda) {
+			posX++;
+		}else{
+			posX--;
+		}
+		 */
+		miSprite.setPosition(posX, 350);//cambiamos la posicion
+		miSprite.rotate(90);
 
 		batch.end();	//FIn de los elementos
+
+		//ESCUCHA DEL TECLADO
+		boolean derecha = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
+		boolean izquierda = Gdx.input.isKeyPressed(Input.Keys.LEFT);
+		boolean shift = Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT);
+
+		float tiempo = Gdx.graphics.getDeltaTime();	//Nos da el tiempo en que pasa por refresco de imagen
+
+		//MOVIMIENTO DEL SPRITE
+		if (izquierda) {
+
+			//COmprobamos si ya pasaron 1 segundos
+			if (tiempoT>=1) {
+				velocidad = 300;
+			}else{
+				tiempoT += tiempo;
+				velocidad = 100;
+			}
+
+			//Decrementamos la posicion en x
+			posX-= velocidad * tiempo;
+		}else if (derecha) {
+			//COmprobamos si ya pasaron 1 segundos
+			if (tiempoT>=1) {
+				velocidad = 300;
+			}else{
+				tiempoT += tiempo;
+				velocidad = 100;
+			}
+
+			//incrementamos la poscion en x
+			posX+= velocidad * tiempo;
+		}else{
+			tiempoT = 0;
+		}
+
+		System.out.println(tiempoT);
+
+		miSprite.setPosition(posX, 350);
+
+		//TAREA INCREMENTAR LA VELOCIDAD DEL MOVIMIENTO DEL MONO DESPUES DE UN TIEMPO Y CUANDO LLEGUE A UNA VELOCIDAD
+		//ESTABLECIDA, NO INCREMENTAR LA VELOCIDAD
 	}
 
 	@Override
